2319aa263e056fcdce58747205aa6b1c742888bc
Apollyonn <jellefenxd@gmail.com>
2018-10-01 01:47:20 +0300

    Fix #1

--- ElvUI_CustomTweaks/Categories/chat/MaxLines.lua
+++ ElvUI_CustomTweaks/Categories/chat/MaxLines.lua
@@ -4,10 +4,9 @@ local CT = E:GetModule("CustomTweaks")
 local isEnabled = E.private["chat"].enable and E.private["CustomTweaks"] and E.private["CustomTweaks"]["ChatMaxLines"] and true or false
 
 local _G = _G
-local pairs = pairs
 
 P["CustomTweaks"]["ChatMaxLines"] = {
-	["MaxLines"] = 128,
+	["MaxLines"] = 128
 }
 
 local UpdateMaxLines
@@ -24,25 +23,25 @@ local function ConfigTable()
 				name = L["Max Lines"],
 				desc = L["The max amount of messages kept in chat before they get replaced by new messages. Changing this setting will clear the chat in all windows."],
 				disabled = function() return not isEnabled end,
-				min = 1, max = 5000, step = 1,
-			},
-		},
+				min = 1, max = 5000, step = 1
+			}
+		}
 	}
 end
 CT.Configs["ChatMaxLines"] = ConfigTable
-if not isEnabled then return; end
+if not isEnabled then return end
 
 function UpdateMaxLines(self)
-	for _, frameName in pairs(CHAT_FRAMES) do
-		local frame = _G[frameName]
-		frame:SetMaxLines(E.db.CustomTweaks.ChatMaxLines.MaxLines)
+	for i = 1, NUM_CHAT_WINDOWS do
+		chat = _G["ChatFrame"..i]
+		chat:SetMaxLines(E.db.CustomTweaks.ChatMaxLines.MaxLines)
 	end
 
 	--Re-display chat history since it was all cleared when calling :SetMaxLines
 	if E.db.chat.chatHistory then
-		self.SoundPlayed = true;
-		self:DisplayChatHistory()
-		self.SoundPlayed = nil;
+		CH.SoundPlayed = true
+		CH:DisplayChatHistory()
+		CH.SoundPlayed = nil
 	end
 end
 hooksecurefunc(CH, "Initialize", UpdateMaxLines)
